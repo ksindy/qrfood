914188659fcac418568933294206b302ab22b79f
ksindy <karlysindy@gmail.com>
2023-04-29 08:16:55 -0400

    if no item_id in db go to add endpoint

--- main.py
+++ main.py
@@ -275,6 +275,6 @@ async def handle_qr_scan(item_id: str):
         return RedirectResponse(url=f"/{item_id}/view/")
     else:
         # Add the new UUID to the database before redirecting to the update page
-        return RedirectResponse(url=f"/{item_id}/update/")
+        return RedirectResponse(url=f"/{item_id}/add/")
 
 
