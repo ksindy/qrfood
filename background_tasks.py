6374a8524da175b47532cb77c8bdc642a5d34afa
ksindy <karlysindy@gmail.com>
2023-05-03 09:15:27 -0400

    adding alerts

--- background_tasks.py
+++ background_tasks.py
@@ -1,24 +1,28 @@
 # background_tasks.py
 from database import connect_to_db, check_date_range
-from alert import send_slack_alert
 import time
 from fastapi import BackgroundTasks, FastAPI
 
 app = FastAPI()
+def write_notification(email: str, message=""):
+    with open("log.txt", mode="w") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
 
 async def check_database_task(days_range):
     while True:
         conn = connect_to_db()
-        results = check_date_range(conn, days_range)
+        results = check_date_range(conn, 7)
         conn.close()
 
         if results:
+            print("Found results!")
             for result in results:
                 message = f"Alert: A date within the specified range was found: {result}"
-                send_slack_alert(message)
+                write_notification("karlysindy@gmail.com", message)
 
         time.sleep(24 * 60 * 60)  # Sleep for a day
 
-@app.on_event("startup")
-async def on_startup(background_tasks: BackgroundTasks):
-    background_tasks.add_task(check_database_task(days_range=7))  # Replace 7 with the desired range in days
+@app.post("/send-notification/{email}")
+async def on_startup(email:str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(check_database_task, email)  # Replace 7 with the desired range in days
--- database.py
+++ database.py
@@ -3,8 +3,8 @@ import os
 import psycopg2
 from datetime import datetime, timedelta
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
+#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = "postgres://frdzidjeclcxty:16551cea6bdcb55a4b5033306cd719c6cd73163cfc03e202c0500553e92ad12a@ec2-3-234-204-26.compute-1.amazonaws.com:5432/d521nossmt6tfl"
 def connect_to_db():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     return conn
