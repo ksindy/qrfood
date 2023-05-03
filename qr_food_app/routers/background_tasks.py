137ddced510d7ee8a9f0aa222590df54a5479301
ksindy <karlysindy@gmail.com>
2023-05-03 12:41:34 -0400

    add twilio alert

--- qr_food_app/routers/background_tasks.py
+++ qr_food_app/routers/background_tasks.py
@@ -2,27 +2,36 @@
 from ..database import connect_to_db, check_date_range
 from fastapi import BackgroundTasks, FastAPI
 from fastapi import APIRouter
+import os
+import twilio
+from twilio.rest import Client
 
 router = APIRouter()
 
 app = FastAPI()
-def write_notification(email: str, message=""):
-    with open("log.txt", mode="w") as email_file:
-        content = f"notification for {email}: {message}"
-        email_file.write(content)
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_PHONE_NUMBER = os.environ['TWILIO_PHONE_NUMBER']
 
-@router.get("/send-notification/{email}/")
-async def send_notification(email: str, background_tasks: BackgroundTasks):
+def send_text_alert(to_phone_number, message):
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body=message,
+        from_=TWILIO_PHONE_NUMBER,
+        to=to_phone_number
+    )
+
+@router.get("/send-notification/{user_phone_number}/")
+async def send_notification(user_phone_number: str, background_tasks: BackgroundTasks):
     while True:
         conn = connect_to_db()
         results = check_date_range(conn, 7)
         conn.close()
-
         if results:
             print("Found results!")
             for result in results:
                 message = f"Alert: A date within the specified range was found: {result}"
-                write_notification(email, message)
+                send_text_alert(user_phone_number, message)
 
-        background_tasks.add_task(write_notification, email, message="some notification")
-        return {"message": "Notification sent in the background"}
+        # background_tasks.add_task(write_notification, email, message="some notification")
+        # return {"message": "Notification sent in the background"}
